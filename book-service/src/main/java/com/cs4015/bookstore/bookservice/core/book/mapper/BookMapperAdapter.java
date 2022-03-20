74981bebdb9b7e5afbe58957bb8ecde7c54db50d
QihaoGuo <56976451+QihaoGuo@users.noreply.github.com>
2022-03-20 15:16:19 -0300

    Add files via upload

--- book-service/src/main/java/com/cs4015/bookstore/bookservice/core/book/mapper/BookMapperAdapter.java
+++ book-service/src/main/java/com/cs4015/bookstore/bookservice/core/book/mapper/BookMapperAdapter.java
@@ -2,18 +2,11 @@ package com.cs4015.bookstore.bookservice.core.book.mapper;
 
 import com.cs4015.bookstore.api.core.book.models.*;
 import com.cs4015.bookstore.bookservice.core.book.model.BookEntity;
-import com.cs4015.bookstore.bookservice.core.book.model.DigitalBookEntity;
-import com.cs4015.bookstore.bookservice.core.book.model.HardCoverBookEntity;
-import com.cs4015.bookstore.bookservice.core.book.model.PaperBackBookEntity;
-import org.mapstruct.ObjectFactory;
-import org.springframework.stereotype.Component;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Optional;
-import java.util.stream.Collectors;
-public class BookMapperAdapter implements Mapper {
+
+public class BookMapperAdapter implements MyBookMapper {
     ApiFactory apifactory;
     EntityFactory entityfactory;
 
