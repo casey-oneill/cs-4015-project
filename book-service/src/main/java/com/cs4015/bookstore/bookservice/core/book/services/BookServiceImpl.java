3050fb040652c472df6a993910fd0332ea891133
QihaoGuo <56976451+QihaoGuo@users.noreply.github.com>
2022-03-20 15:17:19 -0300

    Add files via upload

--- book-service/src/main/java/com/cs4015/bookstore/bookservice/core/book/services/BookServiceImpl.java
+++ book-service/src/main/java/com/cs4015/bookstore/bookservice/core/book/services/BookServiceImpl.java
@@ -2,53 +2,41 @@
 package com.cs4015.bookstore.bookservice.core.book.services;
 
 import com.cs4015.bookstore.api.core.book.models.Book;
-import com.cs4015.bookstore.api.core.book.models.BookType;
-import com.cs4015.bookstore.api.core.book.models.Condition;
-import com.cs4015.bookstore.api.core.book.models.PaperBackBook;
 import com.cs4015.bookstore.api.core.book.services.BookService;
 import com.cs4015.bookstore.api.exceptions.BadRequestException;
 import com.cs4015.bookstore.api.exceptions.InvalidInputException;
 import com.cs4015.bookstore.api.exceptions.NotFoundException;
 import com.cs4015.bookstore.bookservice.core.book.manager.BookManager;
-import com.cs4015.bookstore.bookservice.core.book.mapper.BookApiToEntityMapper;
-import com.cs4015.bookstore.bookservice.core.book.mapper.BookEntityToApiMapper;
-import com.cs4015.bookstore.bookservice.core.book.mapper.BookMapper;
+import com.cs4015.bookstore.bookservice.core.book.mapper.BookMapperAdapter;
 import com.cs4015.bookstore.bookservice.core.book.model.BookEntity;
 import com.cs4015.bookstore.bookservice.core.book.repository.BookRepository;
 import com.cs4015.bookstore.util.ServiceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DuplicateKeyException;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RestController;
 
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class BookServiceImpl implements BookService {
 
     private static final Logger logger = LoggerFactory.getLogger(BookServiceImpl.class);
     private final BookRepository repository;
-    private final BookMapper bookMapper;
+    private final BookMapperAdapter bookMapperAdapter;
     private final BookManager bookManger;
 
     private final ServiceUtil serviceUtil;
 
     @Autowired
-    public BookServiceImpl(BookRepository bookRepository, BookMapper bookMapper, BookManager bookManger, ServiceUtil serviceUtil){
+    public BookServiceImpl(BookRepository bookRepository, BookMapperAdapter bookMapperAdapter, BookManager bookManger, ServiceUtil serviceUtil){
         this.repository = bookRepository;
-        this.bookMapper = bookMapper;
+        this.bookMapperAdapter = bookMapperAdapter;
         this.serviceUtil = serviceUtil;
         this.bookManger = bookManger;
     }
-    
+
     @Override
     public Book getBook(long bookId) {
         logger.info("/books return a book.");
